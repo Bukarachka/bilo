fe6bb465ff73b83bf69a2a8257d2bfb2de24c7a6
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-02-26 16:59:01 +0200

    update view

--- src/views/home/download/Dowload.js
+++ src/views/home/download/Dowload.js
@@ -3,18 +3,19 @@ import React from 'react';
 import './Download.css';
 
 import phonesImage from '../../../assets/img/home_download_phones.png';
+import quoteImage from '../../../assets/img/home_download_quote.png';
 
 class Download extends React.PureComponent{
     render(){
         return<div className="HomeDownload">
-            <div>
+            <div className="Image">
                 <img src={phonesImage} alt=""/>
             </div>
-            <div>
-                <a>interested in the app</a>
+            <div className="Content">
+                <a href="/">interested in the app</a>
                 <h1>Available For Your Cute Smartphone & Tab.</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
-                <div>
+                <div className="Buttons">
                     <button>
                         <p>Get It On</p>
                         <h2>Google Play</h2>
@@ -24,10 +25,10 @@ class Download extends React.PureComponent{
                         <h2>Appple Store</h2>
                     </button>
                 </div>
-                <div>
-                    <img />
-                    <h2></h2>
-                    <a></a>
+                <div className="Quote">
+                    <img src={quoteImage} alt="" />
+                    <h2>2M Happy Users</h2>
+                    <a href="/">Read More</a>
                 </div>
             </div>
         </div>
