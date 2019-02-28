dc2c615dc7b262d3db52f8fbdddf94ae07736c35
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-02-28 18:59:28 +0200

    add video layer

--- src/views/home/video/Video.js
+++ src/views/home/video/Video.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import './Video.css';
+
+import placeholder from '../../../assets/img/home_video_placeholder.png';
+import playImage from '../../../assets/img/home_video_play.png';
+
 class Video extends React.PureComponent{
     render(){
-        return<div>
-            Video
+        return<div className="HomeVideo">
+            <div>
+            <a href="/">intro video</a>
+                <h1>High Quality Video Chating & Conference</h1>
+                <div className="Video">
+                    <img src={placeholder} alt=""/>
+                    <div className="Overlay">
+                        <p>Play</p>
+                    </div>
+                </div>
+            </div>
         </div>
     }
 }
